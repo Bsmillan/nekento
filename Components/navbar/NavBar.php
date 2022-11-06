3f359fc0606f369de05c8c63f7476929bd7cc6e5
Brahiam Millan <brahiam@anteia.co>
2022-11-06 13:11:19 -0500

    cambios

--- Components/navbar/NavBar.php
+++ Components/navbar/NavBar.php
@@ -4,40 +4,59 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./NavBar.css">
-  <link rel="stylesheet" href="../../css/style.css">
+  <link rel="stylesheet" href="../Components/navbar/NavBar.css">
+  <link rel="stylesheet" href="../Components/footer/footer.css">
+  <link rel="stylesheet" href="../css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="../assets/favicon.ico" type="image/x-icon">
+  
   <title>Nekento</title>
 </head>
 <body>
   <nav class="navbar navbar-expand-sm">
-      <div class="container">
-      <a class="navbar-brand" href="../pages/Login/login.php">Nekento</a>
-      <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-        aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container">
+      <a class="navbar-brand" href="../pages/index.php"><img src="../assets/logo3.png" class="img-fluid" alt="" width="250em"></a>
+      <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="collapsibleNavId">
-        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+        <ul class="navbar-nav mt-2 mt-lg-0 ">
           <li class="nav-item">
-            <a class="nav-link active" href="" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdownId">
-              <a class="dropdown-item" href="#">Action 1</a>
-              <a class="dropdown-item" href="#">Action 2</a>
-            </div>
+          <form class="d-flex my-4 my-lg-0">
+            <input id="h" class="form-control" type="text" placeholder="Search">
+            <button id="c" type="button" class="btn">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
+                  </svg>
+                  <span class="visually-hidden">Button</span>
+                </button>
+          </form>
           </li>
         </ul>
-        <form class="d-flex my-2 my-lg-0">
-          <input class="form-control me-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <ul class="navbar-nav me-2 mt-2 mt-lg-0">
+          <li class="nav-item dropdown">
+            <a id="btn" class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categorias</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownId">
+              <a class="dropdown-item" href="#">Sonido</a>
+              <a class="dropdown-item" href="#">Entretenimiento</a>
+              <a class="dropdown-item" href="#">Decoracion</a>
+              <a class="dropdown-item" href="#">Catering</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a id="btn" class="nav-link" href="../pages/Register/cart.php">Carrito</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-2 mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a id="btn" class="nav-link" href="../pages/Register/register.php">Crea tu cuenta</a>
+          </li>
+          <li class="nav-item">
+            <a id="btn" class="nav-link" href="../pages/Login/login.php">Inicia sesion</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
--- pages/Register/register.php
+++ pages/Register/register.php
@@ -1,100 +1,91 @@
 <!DOCTYPE html>
 <html lang="es">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Nekento</title>
-  <link rel="stylesheet" href="/css/style.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="card login">
-        <div class="logo">
-          <img class="rounded " src="/assets/logo.png" alt="logo" width="150">
-        </div>
-        <div class="card-header text-center">
-          <h3>Registrate</h3>
-        </div>
-        <div class="card-body">
-          <form action="javascript:void(0)" method="post">
-            <div class="mb-3">
-              <label for="" class="form-label">Nombre</label>
-              <input type="text" class="form-control " required placeholder="Nombre" name="name" id="name"
-                aria-describedby="name" placeholder="Juanito Perez">
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Numero de celular</label>
-              <input type="tel" class="form-control" name="phone_Num" id="phone_Num" aria-describedby="phone_Num"
-                placeholder="">
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Direccion</label>
-              <input type="text" class="form-control" name="address" id="address" aria-describedby="address"
-                placeholder="">
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Tipo Documento</label>
-              <select class="form-select form-select-lg" name="type_doc" id="type_doc" required>
-                <option selected>Eliga una opcion</option>
-                <option value="0">Cedula</option>
-                <option value="1">Pasaporte</option>
-                <option value="2">Cedula extranjeria</option>
-              </select>
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Numero documento</label>
-              <input type="number" class="form-control" name="num_doc" id="num_doc" aria-describedby="num_doc"
-                placeholder="" required>
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Password</label>
-              <input type="password" class="form-control" name="password" id="password" placeholder="" required>
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Confirm Password</label>
-              <input type="password" class="form-control" name="confirm_pass" id="confirm_pass" placeholder="" required>
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Email</label>
-              <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId"
-                placeholder="abc@mail.com">
-              <small id="emailHelpId" class="form-text text-muted">Help text</small>
-            </div>
-            <div class="mb-3">
-              <label for="" class="form-label">Email</label>
-              <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId"
-                placeholder="abc@mail.com">
-              <small id="emailHelpId" class="form-text text-muted">Help text</small>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="" value="option1">
-              <a href=""><label class="form-check-label" for="">Terminos y condiciones</label></a>
-            </div>
-            <div class="text-center">
-              <button type="submit" class="btn btn-primary">Login</button>
-            </div>
-          </form>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nekento</title>
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../pages/Register/Register.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="shortcut icon" href="../../assets/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../../assets/favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="card" id="register">
+          <div class="logo">
+            <a href="../index.php"><img class="rounded" src="../../assets/logo.png" alt="logo" width="150"></a>
+          </div>
+          <div class="text-center pt-4">
+            <h3>Crea tu cuenta</h3>
+          </div>
+          <div class="card-body">
+            <form action="javascript:void(0)" method="post">
+              <div class="mb-3">
+                <label for="" class="form-label">Nombre</label>
+                <input type="text" class="form-control " required placeholder="Nombre" name="name" id="name"
+                  aria-describedby="name" placeholder="Juanito Perez">
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Numero de celular</label>
+                <input type="tel" class="form-control" name="phone_Num" id="phone_Num" aria-describedby="phone_Num"
+                  placeholder="">
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Direccion</label>
+                <input type="text" class="form-control" name="address" id="address" aria-describedby="address"
+                  placeholder="">
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Tipo Documento</label>
+                <select class="form-select form-select-lg" name="type_doc" id="type_doc" required>
+                  <option selected>Eliga una opcion</option>
+                  <option value="0">Cedula</option>
+                  <option value="1">Pasaporte</option>
+                  <option value="2">Cedula extranjeria</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Numero documento</label>
+                <input type="number" class="form-control" name="num_doc" id="num_doc" aria-describedby="num_doc"
+                  placeholder="" required>
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="" required>
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" name="confirm_pass" id="confirm_pass" placeholder="" required>
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Email</label>
+                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId"
+                  placeholder="abc@mail.com">
+              </div>
+              <div class="mb-3">
+                <label for="" class="form-label">Email</label>
+                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId"
+                  placeholder="abc@mail.com">
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="" value="option1">
+                <a href=""><label class="form-check-label" for="">Terminos y condiciones</label></a>
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn" id="btn">Crear cuenta</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
+  </body>
+  <div class="text-center p-4" style="background-color: black;">
+    © 2022 Copyright:
+    <a id="social-link" class="text-reset fw-bold" href="https://nekento.com/">Nekento</a>
   </div>
-</body>
-
-
 </html>
-Nombre
-np telefono
-direccion
-tipo doc
-no doc
-Password
-password confirmation
-email
-email confirmation
-T&C
